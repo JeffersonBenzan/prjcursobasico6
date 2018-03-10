cf777348fb57d9a5095ec831289a5cafdacda70b
Emmanuel Polanco Jimenez <emmapj18@gmail.com>
2018-03-10 12:50:41 -0400

    Adding menu to MainActivity

--- app/src/main/java/com/example/jean/prj_cursobasico_6/MainActivity.java
+++ app/src/main/java/com/example/jean/prj_cursobasico_6/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.jean.prj_cursobasico_6;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.item_about:
+                break;
+            case R.id.item_close:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }

e6df228254e603ec960c60cbba0e3733c51e4bdd
Emmanuel Polanco Jimenez <emmapj18@gmail.com>
2018-03-11 15:16:05 -0400

    Eliminando Warnings

--- app/src/main/java/com/example/jean/prj_cursobasico_6/utilities.java
+++ app/src/main/java/com/example/jean/prj_cursobasico_6/utilities.java
@@ -4,28 +4,11 @@ package com.example.jean.prj_cursobasico_6;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
-public class utilities {
+class utilities {
 
-    public static boolean isNullOrWhitespace(String s) {
-        return s == null || isWhitespace(s);
+    static double redondear(double value) {
 
-    }
-    private static boolean isWhitespace(String s) {
-        int length = s.length();
-        if (length > 0) {
-            for (int i = 0; i < length; i++) {
-                if (!Character.isWhitespace(s.charAt(i))) {
-                    return false;
-                }
-            }
-            return true;
-        }
-        return false;
-    }
+        return new BigDecimal(value).setScale(2, RoundingMode.HALF_UP).doubleValue();
 
-    public static double round(double value, int places) {
-        if (places < 0) throw new IllegalArgumentException();
-
-        return new BigDecimal(value).setScale(places, RoundingMode.HALF_UP).doubleValue();
     }
 }
